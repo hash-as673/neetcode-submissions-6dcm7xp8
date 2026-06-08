7b597ace14ec6387afb403aca87f31643c613dfa
Aditya Singh <100013000+hash-as673@users.noreply.github.com>
2026-06-08 21:48:57 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.py
@@ -0,0 +1,28 @@
+class MinStack:
+
+    def __init__(self):
+        self.min_stack = []
+        self.stack = []
+        
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if not self.min_stack:
+            self.min_stack.append(val)
+        else:
+            self.min_stack.append(min(val,self.min_stack[-1]))
+            
+
+    def pop(self) -> None:
+        self.min_stack.pop()
+        self.stack.pop()
+
+    def top(self) -> int:
+        if not self.stack:
+            return None
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        if not self.min_stack:
+            return None
+        return self.min_stack[-1]
